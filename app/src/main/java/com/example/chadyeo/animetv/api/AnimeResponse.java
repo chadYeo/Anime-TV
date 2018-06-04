bc6dd056b6704ab56dda7a27587717a0c92e9e91
Chad Yeo <chad.yeo.cy@gmail.com>
2018-06-03 22:41:32 -0400

    Added AnimeResponse based on AniList Api Docs

---
+++ app/src/main/java/com/example/chadyeo/animetv/api/AnimeResponse.java
@@ -0,0 +1,129 @@
+package com.example.chadyeo.animetv.api;
+
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class AnimeResponse {
+
+    public ArrayList<Anime> results;
+
+    public static class Anime implements Parcelable {
+
+        /**
+         * Series Model
+         */
+        @SerializedName("series_type")
+        public String seriesType;
+
+        @SerializedName("title_english")
+        public String titleEnglish;
+
+        @SerializedName("season")
+        public int season;
+
+        @SerializedName("description")
+        public String description;
+
+        @SerializedName("genres")
+        public String genres;
+
+        @SerializedName("adult")
+        public Byte adult; // Respond will be in boolean : set true, byte ==1 and set false, byte ==2
+
+        @SerializedName("average_score")
+        public double averageScore;
+
+        @SerializedName("popularity")
+        public int popularity;
+
+        @SerializedName("image_url_sml")
+        public String image_url_sml;
+
+        @SerializedName("image_url_med")
+        public String image_url_med;
+
+        @SerializedName("image_url_lge")
+        public String image_url_lge;
+
+        @SerializedName("image_url_banner")
+        public String image_url_banner;
+
+        /**
+         * Anime Model
+         */
+        @SerializedName("total_episodes")
+        public int totalEpisodes;
+
+        @SerializedName("youtube_id")
+        public String youtube_id;
+
+        /**
+         * Manga Model
+         */
+        @SerializedName("total_chapters")
+        public int total_chapters;
+
+        @SerializedName("total_volume")
+        public int total_volume;
+
+
+        public static final Parcelable.Creator<Anime> CREATOR = new Parcelable.Creator<Anime>() {
+            public Anime createFromParcel(Parcel in) {
+                return new Anime(in);
+            }
+
+            public Anime[] newArray(int size) {
+                return new Anime[size];
+            }
+        };
+
+        public Anime(Parcel in) {
+            seriesType = in.readString();
+            titleEnglish = in.readString();
+            season = in.readInt();
+            description = in.readString();
+            genres = in.readString();
+            adult = in.readByte();
+            averageScore = in.readDouble();
+            popularity = in.readInt();
+            image_url_sml = in.readString();
+            image_url_med = in.readString();
+            image_url_lge = in.readString();
+            image_url_banner = in.readString();
+            totalEpisodes = in.readInt();
+            youtube_id = in.readString();
+            total_chapters = in.readInt();
+            total_volume = in.readInt();
+        }
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            out.writeString(seriesType);
+            out.writeString(titleEnglish);
+            out.writeInt(season);
+            out.writeString(description);
+            out.writeString(genres);
+            out.writeByte(adult);
+            out.writeDouble(averageScore);
+            out.writeInt(popularity);
+            out.writeString(image_url_sml);
+            out.writeString(image_url_med);
+            out.writeString(image_url_lge);
+            out.writeString(image_url_banner);
+            out.writeInt(totalEpisodes);
+            out.writeString(youtube_id);
+            out.writeInt(total_chapters);
+            out.writeInt(total_volume);
+        }
+    }
+}
--- app/src/main/java/com/example/chadyeo/animetv/fragments/MainFragment.java
+++ app/src/main/java/com/example/chadyeo/animetv/fragments/MainFragment.java
@@ -1,19 +1,28 @@
 package com.example.chadyeo.animetv.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.chadyeo.animetv.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainFragment extends Fragment {
 
+    @BindView(R.id.main_frag_progressBar) ProgressBar progressBar;
+    @BindView(R.id.error_text_view) TextView errorTextView;
+    @BindView(R.id.anime_recyclerView) RecyclerView recyclerView;
 
     public MainFragment() {
         // Required empty public constructor
@@ -24,7 +33,48 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+
+
+        return view;
     }
 
+
+    // Anime RecyclerView adapter class
+    private static class AnimeAdapter extends RecyclerView.Adapter<AnimeViewHolder> {
+
+        final private Context mContet;
+
+        private AnimeAdapter(Context mContet) {
+            this.mContet = mContet;
+        }
+
+        @NonNull
+        @Override
+        public AnimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull AnimeViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    // Anime view holder class
+    private static class AnimeViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.anime_item_imageView) ImageView mAnimeImageView;
+
+        public AnimeViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 }

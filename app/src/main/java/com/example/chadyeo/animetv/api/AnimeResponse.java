9dce464f4551fb0bb8baf069a39187ec2b839edd
Chad Yeo <chad.yeo.cy@gmail.com>
2018-06-03 23:12:33 -0400

    Added AnimeResponse based on AniList Api docs

--- app/src/main/java/com/example/chadyeo/animetv/api/AnimeResponse.java
+++ app/src/main/java/com/example/chadyeo/animetv/api/AnimeResponse.java
@@ -12,46 +12,7 @@ public class AnimeResponse {
 
     public ArrayList<Anime> results;
 
-    public static class Anime implements Parcelable {
-
-        /**
-         * Series Model
-         */
-        @SerializedName("series_type")
-        public String seriesType;
-
-        @SerializedName("title_english")
-        public String titleEnglish;
-
-        @SerializedName("season")
-        public int season;
-
-        @SerializedName("description")
-        public String description;
-
-        @SerializedName("genres")
-        public String genres;
-
-        @SerializedName("adult")
-        public Byte adult; // Respond will be in boolean : set true, byte ==1 and set false, byte ==2
-
-        @SerializedName("average_score")
-        public double averageScore;
-
-        @SerializedName("popularity")
-        public int popularity;
-
-        @SerializedName("image_url_sml")
-        public String image_url_sml;
-
-        @SerializedName("image_url_med")
-        public String image_url_med;
-
-        @SerializedName("image_url_lge")
-        public String image_url_lge;
-
-        @SerializedName("image_url_banner")
-        public String image_url_banner;
+    private static class Anime implements Parcelable {
 
         /**
          * Anime Model
@@ -62,44 +23,22 @@ public class AnimeResponse {
         @SerializedName("youtube_id")
         public String youtube_id;
 
-        /**
-         * Manga Model
-         */
-        @SerializedName("total_chapters")
-        public int total_chapters;
+        protected Anime(Parcel in) {
+            totalEpisodes = in.readInt();
+            youtube_id = in.readString();
+        }
 
-        @SerializedName("total_volume")
-        public int total_volume;
-
-
-        public static final Parcelable.Creator<Anime> CREATOR = new Parcelable.Creator<Anime>() {
+        public static final Creator<Anime> CREATOR = new Creator<Anime>() {
+            @Override
             public Anime createFromParcel(Parcel in) {
                 return new Anime(in);
             }
 
+            @Override
             public Anime[] newArray(int size) {
                 return new Anime[size];
             }
         };
-
-        public Anime(Parcel in) {
-            seriesType = in.readString();
-            titleEnglish = in.readString();
-            season = in.readInt();
-            description = in.readString();
-            genres = in.readString();
-            adult = in.readByte();
-            averageScore = in.readDouble();
-            popularity = in.readInt();
-            image_url_sml = in.readString();
-            image_url_med = in.readString();
-            image_url_lge = in.readString();
-            image_url_banner = in.readString();
-            totalEpisodes = in.readInt();
-            youtube_id = in.readString();
-            total_chapters = in.readInt();
-            total_volume = in.readInt();
-        }
 
         @Override
         public int describeContents() {
@@ -108,22 +47,8 @@ public class AnimeResponse {
 
         @Override
         public void writeToParcel(Parcel out, int flags) {
-            out.writeString(seriesType);
-            out.writeString(titleEnglish);
-            out.writeInt(season);
-            out.writeString(description);
-            out.writeString(genres);
-            out.writeByte(adult);
-            out.writeDouble(averageScore);
-            out.writeInt(popularity);
-            out.writeString(image_url_sml);
-            out.writeString(image_url_med);
-            out.writeString(image_url_lge);
-            out.writeString(image_url_banner);
             out.writeInt(totalEpisodes);
             out.writeString(youtube_id);
-            out.writeInt(total_chapters);
-            out.writeInt(total_volume);
         }
     }
 }

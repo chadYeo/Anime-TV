7f6806822ee2165e15c8b17216819b69f5b6b452
Chad Yeo <chad.yeo@sisa.samsung.com>
2018-07-20 20:56:43 -0400

    Updated Adapter

--- app/src/main/java/com/example/chadyeo/animetv/api/AllAnimeRecyclerViewAdapter.java
+++ app/src/main/java/com/example/chadyeo/animetv/api/AllAnimeRecyclerViewAdapter.java
@@ -1,18 +1,66 @@
 package com.example.chadyeo.animetv.api;
 
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.chadyeo.animetv.R;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class AllAnimeRecyclerViewAdapter extends RecyclerView.Adapter<AllAnimeRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Anime> mValues = new ArrayList<>();
+
+    public AllAnimeRecyclerViewAdapter(ArrayList<Anime> items) {
+        mValues.addAll(items);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.anime_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.anime_title_textView.setText(holder.mItem.getTitle_romaji());
+        holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes());
+        holder.anime_type_textView.setText(holder.mItem.getType());
+
+
+        if (holder.mItem.getTotal_episodes() > 1) {
+            holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes() + " eps");
+        } else if (holder.mItem.getTotal_episodes() = 1){
+            holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes() + " ep");
+        } else {
+            holder.anime_episodes_textView.setText("? eps");
+        }
+
+        String url = holder.mItem.getImage_url_lge();
+        if (url == null) {
+            url = holder.mItem.getImage_url_med();
+        }
+        if (url == null) {
+            url = holder.mItem.getImage_url_sml();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public View mView;

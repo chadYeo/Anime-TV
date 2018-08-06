ffe64b91e6478e4e7540a3f0604a915b2b5bd46a
Chad Yeo <chad.yeo@sisa.samsung.com>
2018-08-06 15:16:00 -0400

    Added TVAnime Fragment and adapter

---
+++ app/src/main/java/com/example/chadyeo/animetv/adapters/TVAnimeItemRecyclerViewAdapter.java
@@ -0,0 +1,138 @@
+package com.example.chadyeo.animetv.adapters;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.chadyeo.animetv.R;
+import com.example.chadyeo.animetv.api.Anime;
+import com.example.chadyeo.animetv.api.AnimeList;
+import com.example.chadyeo.animetv.fragments.TVAnimeFragment;
+
+import java.util.ArrayList;
+
+public class TVAnimeItemRecyclerViewAdapter
+        extends RecyclerView.Adapter<TVAnimeItemRecyclerViewAdapter.ViewHolder> {
+
+    private ArrayList<Anime> mValues = new ArrayList<>();
+    private TVAnimeFragment.OnTVAnimeFragmentInteractionListener mListener;
+
+    public TVAnimeItemRecyclerViewAdapter(ArrayList<Anime> items, TVAnimeFragment.OnTVAnimeFragmentInteractionListener listener) {
+        mValues.addAll(items);
+        mListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.anime_item, parent, false);
+        return new TVAnimeItemRecyclerViewAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.anime_title_textView.setText(holder.mItem.getTitle_romaji());
+        holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes());
+        holder.anime_type_textView.setText(holder.mItem.getType());
+
+
+        if (holder.mItem.getTotal_episodes() > 1) {
+            holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes() + " eps");
+        } else if (holder.mItem.getTotal_episodes() == 1){
+            holder.anime_episodes_textView.setText(holder.mItem.getTotal_episodes() + " ep");
+        } else {
+            holder.anime_episodes_textView.setText("? eps");
+        }
+
+        String url = holder.mItem.getImage_url_lge();
+        if (url == null) {
+            url = holder.mItem.getImage_url_med();
+        }
+        if (url == null) {
+            url = holder.mItem.getImage_url_sml();
+        }
+        Glide.with(holder.anime_item_imageView.getContext())
+                .load(url)
+                .centerCrop()
+                .into(holder.anime_item_imageView);
+        holder.anime_item_imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ToDo: Intent to Detail
+                mListener.onTVAnimeFragmentInteraction(holder.mItem);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private synchronized void update(ArrayList<Anime> temp) {
+        mValues.clear();
+        mValues.addAll(temp);
+    }
+
+    public void reloadDataSource(AnimeList newUserList) {
+        update(newUserList.getAll());
+    }
+
+    public void changeDataSource(AnimeList newUserList) {
+        ArrayList<Anime> temp = new ArrayList<>();
+        for (int i = 0; i < Math.min(newUserList.getAll().size(), 20); i++) {
+            temp.add(newUserList.getAll().get(i));
+            update(temp);
+        }
+    }
+
+    public void clearBitmapCache(Context c) {
+        Glide.get(c).clearMemory();
+        Log.w("Memory Cleared: ", "Glid Memory is Cleared");
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public View mView;
+        public Anime mItem;
+
+        public CardView anime_item_cardView;
+        public ImageView anime_item_imageView;
+        public TextView anime_title_textView;
+        public TextView anime_episodes_textView;
+        public TextView anime_type_textView;
+
+        //@BindView(R.id.anime_item_cardView)CardView anime_item_cardView;
+        //@BindView(R.id.anime_item_imageView) ImageView anime_item_imageView;
+        //@BindView(R.id.anime_title_textView) TextView anime_title_textView;
+        //@BindView(R.id.anime_episodes_textView) TextView anime_episodes_textView;
+        //@BindView(R.id.anime_type_textView) TextView anime_type_textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            anime_item_cardView = (CardView) view.findViewById(R.id.anime_item_cardView);
+            anime_item_imageView = (ImageView) view.findViewById(R.id.anime_item_imageView);
+            anime_title_textView = (TextView) view.findViewById(R.id.anime_title_textView);
+            anime_episodes_textView = (TextView) view.findViewById(R.id.anime_episodes_textView);
+            anime_type_textView = (TextView) view.findViewById(R.id.anime_type_textView);
+
+            //ButterKnife.bind(this, view);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/chadyeo/animetv/fragments/TVAnimeFragment.java
@@ -0,0 +1,126 @@
+package com.example.chadyeo.animetv.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.chadyeo.animetv.R;
+import com.example.chadyeo.animetv.adapters.MovieAnimeItemRecyclerViewAdapter;
+import com.example.chadyeo.animetv.adapters.TVAnimeItemRecyclerViewAdapter;
+import com.example.chadyeo.animetv.api.Anime;
+import com.example.chadyeo.animetv.utils.ListContent;
+import com.example.chadyeo.animetv.utils.ListOptions;
+
+
+public class TVAnimeFragment extends Fragment {
+
+    private static final String LOG_TAG = TVAnimeFragment.class.getSimpleName();
+    private GridLayoutManager gridLayoutManager;
+    private TVAnimeItemRecyclerViewAdapter adapter;
+    private OnTVAnimeFragmentInteractionListener mListener;
+
+    public TVAnimeFragment() {
+    }
+    public static TVAnimeFragment newInstance(int columnCount) {
+        TVAnimeFragment fragment = new TVAnimeFragment();
+        Bundle args = new Bundle();
+        args.putInt(ListOptions.ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_list, container, false);
+        final RecyclerView list = (RecyclerView) view;
+        list.setHasFixedSize(true);
+        list.setItemViewCacheSize(20);
+        list.setDrawingCacheEnabled(true);
+        list.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            recyclerView.setHasFixedSize(true);
+            gridLayoutManager = new GridLayoutManager(context, 2);
+            recyclerView.setLayoutManager(gridLayoutManager);
+            adapter = new TVAnimeItemRecyclerViewAdapter(ListContent.getList().getMovie(), mListener);
+            adapter.setHasStableIds(true);
+            recyclerView.setAdapter(adapter);
+        }
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnTVAnimeFragmentInteractionListener) {
+            mListener = (OnTVAnimeFragmentInteractionListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapter.notifyDataSetChanged();
+        if (getView() != null) {
+            RecyclerView list = (RecyclerView) getView().findViewById(R.id.anime_recyclerView);
+            list.getLayoutManager().scrollToPosition(0);
+            updateList();
+            Log.d(LOG_TAG, "onResume: Size of Data: " + String.valueOf(adapter.getItemCount()));
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        adapter.clearBitmapCache(this.getContext());
+    }
+
+    public interface OnTVAnimeFragmentInteractionListener {
+        void onTVAnimeFragmentInteraction(Anime item);
+    }
+
+    public void reloadList() {
+        if (adapter != null) {
+            adapter.reloadDataSource(ListContent.getList());
+            adapter.clearBitmapCache(this.getContext());
+            adapter.notifyDataSetChanged();
+            Log.d(LOG_TAG, "reloadList - Size of Data: " + String.valueOf(adapter.getItemCount()));
+        } else {
+            adapter = new TVAnimeItemRecyclerViewAdapter(ListContent.getList().getAll(), mListener);
+        }
+    }
+
+    public void updateList() {
+        if (adapter != null) {
+            adapter.changeDataSource(ListContent.getList());
+            adapter.clearBitmapCache(this.getContext());
+            adapter.notifyDataSetChanged();
+            if (getView() != null) {
+                RecyclerView list = (RecyclerView) getView().findViewById(R.id.anime_recyclerView);
+                list.getLayoutManager().scrollToPosition(0);
+            }
+            Log.d(LOG_TAG, "Size of Data: " + String.valueOf(adapter.getItemCount()));
+        } else {
+            adapter = new TVAnimeItemRecyclerViewAdapter(ListContent.getList().getAll(), mListener);
+            Log.d(LOG_TAG, "Size of Data: " + String.valueOf(adapter.getItemCount()));
+        }
+    }
+}
